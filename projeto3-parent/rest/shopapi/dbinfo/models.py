dc8d941d95734ac47a83cbc252521f25e3cdc8fe
Fábio Cordeiro <fabiogcordeiroo@gmail.com>
2019-12-13 06:38:21 +0000

    endpoints criados, list items

--- projeto3-parent/rest/shopapi/dbinfo/models.py
+++ projeto3-parent/rest/shopapi/dbinfo/models.py
@@ -7,21 +7,17 @@ class Item(models.Model):
     class Meta:
         db_table = 'item'
 
-    
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=5,decimal_places=2)
-    #country = Country()
+    country = models.CharField(max_length=50)
 
 class Country(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=20)
-
-    def serialize(self):
-        c = self
-        return {
-            "id": c.id,
-            "name": c.name,
-        }
 
     class Meta:
         db_table = 'countries'
+
+
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+
--- projeto3-parent/rest/shopapi/statistics/views.py
+++ projeto3-parent/rest/shopapi/statistics/views.py
@@ -11,7 +11,81 @@ from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
 def list_database_countries(request):
-    resources = list(Country.objects.filter())
-    for resource in resources:
-        print(resource.name)
+    country_list = list(Country.objects.filter())
+    countries = ""
+    for country in country_list:
+        countries += country.name + ", "
+    return HttpResponse(countries)
+
+@csrf_exempt
+def list_database_items(request):
+    item_list = list(Item.objects.filter())
+    items = ""
+    for item in item_list:
+        items += item.name + ", "
+    return HttpResponse(items)
+
+@csrf_exempt
+def get_revenue_by_item(request):
+    #TO DO
+    return HttpResponse()
+
+@csrf_exempt
+def get_expenses_by_item(request):
+    #TO DO
+    return HttpResponse()
+
+@csrf_exempt
+def get_profit_by_item(request):
+    #TO DO
+    return HttpResponse()
+
+@csrf_exempt
+def get_total_revenue(request):
+    #TO DO
+    return HttpResponse()
+
+@csrf_exempt
+def get_total_expenses(request):
+    #TO DO
+    return HttpResponse()
+
+@csrf_exempt
+def get_total_profit(request):
+    #TO DO
+    return HttpResponse()
+
+@csrf_exempt
+def get_average_amount_spent_in_purchase_by_item(request):
+    #TO DO
+    return HttpResponse()
+
+@csrf_exempt
+def get_average_amount_spent_in_purchase(request):
+    #TO DO
+    return HttpResponse()
+
+@csrf_exempt
+def get_highest_profit_item(request):
+    #TO DO
+    return HttpResponse()
+
+@csrf_exempt
+def get_total_revenue_time(request):
+    #TO DO
+    return HttpResponse()
+
+@csrf_exempt
+def get_total_expenses_time(request):
+    #TO DO
+    return HttpResponse()
+
+@csrf_exempt
+def get_total_profit_last_time(request):
+    #TO DO
+    return HttpResponse()
+
+@csrf_exempt
+def get_highest_sales_country_by_item(request):
+    #TO DO
     return HttpResponse()
